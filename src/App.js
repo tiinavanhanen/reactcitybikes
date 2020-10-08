b4c004d894fb5e499fdebfe56b9783ae4a4c0560
Tiina Vanhanen <tiina.vanhanen@metropolia.fi>
2020-10-08 16:04:03 +0300

    bike station selection

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Select from 'react-select';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  state = {
+    data: [],
+    chosen: {
+      name: "",
+      freeBikes: "",
+      freeSlots: "",
+      lat: "60.170716",
+      long: "24.941412"
+    },
+  }
+
+  options = [];
+
+  componentDidMount() {
+    axios
+        .get('http://api.citybik.es/v2/networks/citybikes-helsinki')
+        .then(res => {
+          const stations = res.data.network.stations;
+          Array.from(stations).forEach(element => {
+            this.options.push({"value": element.name, "label": element.name,
+                "latitude": element.latitude, "longitude": element.longitude,
+                "free_bikes": element.free_bikes, "empty_slots": element.empty_slots});
+          });
+          console.log(this.options);
+          this.setState({data: stations});
+          console.log(this.state.data);
+        })
+  };
+
+    showData = (e) => {
+        this.setState({
+            chosen: {
+                name: e.value,
+                freeBikes: e.free_bikes,
+                freeSlots: e.empty_slots,
+                lat: e.latitude,
+                long: e.longitude,
+            }
+        })
+    };
+
+  render() {
+    return (
+        <div className="App">
+            <Select
+                placeholder="Select bike station"
+                options={this.options}
+                onChange={this.showData}
+            />
+            <div className="data">
+                <p><b>Selected station: </b> {this.state.chosen.name}</p>
+                <p><b>Free bikes: </b> {this.state.chosen.freeBikes}</p>
+                <p><b>Empty slots: </b> {this.state.chosen.freeSlots}</p>
+            </div>
+        </div>
+    );
+  }
 }
-
-export default App;
